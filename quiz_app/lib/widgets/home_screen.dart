3422d95e0a3b2c719f176bc250e132e4866e6631
Apple <apple@Apples-MacBook-Pro.local>
2024-10-03 20:01:46 +0530

    +finished Home Screen

---
+++ quiz_app/lib/widgets/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Image(
+            image: AssetImage('assets/images/quiz-logo.png'),
+            width: 300,
+            color: Color.fromARGB(147, 222, 220, 219),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
